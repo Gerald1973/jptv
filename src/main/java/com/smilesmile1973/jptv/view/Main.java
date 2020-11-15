b427cade3b32c79707c5e24f7d1ccc18ebc0fc46
gerald1973@gmail.com <private@LAPTOP-CDL-GMA.fritz.box>
2020-11-15 10:22:48 +0100

    Ratio taking in account

--- src/main/java/com/smilesmile1973/jptv/view/Main.java
+++ src/main/java/com/smilesmile1973/jptv/view/Main.java
@@ -15,6 +15,7 @@ import com.smilesmile1973.jptv.Utils;
 import com.smilesmile1973.jptv.event.EventChannel;
 import com.smilesmile1973.jptv.pojo.Channel;
 import com.smilesmile1973.jptv.service.M3UService;
+import com.sun.jna.ptr.IntByReference;
 
 import javafx.application.Application;
 import javafx.beans.value.ChangeListener;
@@ -33,9 +34,11 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
 import javafx.stage.Window;
+import uk.co.caprica.vlcj.binding.LibVlc;
 import uk.co.caprica.vlcj.factory.MediaPlayerFactory;
 import uk.co.caprica.vlcj.player.base.MediaPlayer;
 import uk.co.caprica.vlcj.player.base.MediaPlayerEventAdapter;
@@ -52,26 +55,32 @@ public class Main extends Application {
 	private final MediaPlayerFactory mediaPlayerFactory;
 
 	private EmbeddedMediaPlayer embeddedMediaPlayer;
-	
+
+	private boolean keepRatio = true;
+
 	public Main() {
 		this.mediaPlayerFactory = new MediaPlayerFactory();
 		this.embeddedMediaPlayer = mediaPlayerFactory.mediaPlayers().newEmbeddedMediaPlayer();
 		this.embeddedMediaPlayer.events().addMediaPlayerEventListener(new MediaPlayerEventAdapter() {
 			@Override
 			public void playing(MediaPlayer mediaPlayer) {
-
+				LOG.debug("Media player playing");
+				placeVideoImage(videoImageView, keepRatio);
 			}
 
 			@Override
 			public void paused(MediaPlayer mediaPlayer) {
+				LOG.debug("Media player paused");
 			}
 
 			@Override
 			public void stopped(MediaPlayer mediaPlayer) {
+				LOG.debug("Media player stopped");
 			}
 
 			@Override
 			public void timeChanged(MediaPlayer mediaPlayer, long newTime) {
+				LOG.debug("Media player time changed");
 			}
 		});
 	}
@@ -108,8 +117,8 @@ public class Main extends Application {
 		root.setTop(buildTopPane(owner));
 		Node left = buildLeftPane();
 		Node right = buildCenterPane();
-		SplitPane splitPane = new SplitPane(left,right);
-		splitPane.setDividerPosition(0, Constants.CHANNEL_LIST_WIDTH/Constants.STAGE_WIDTH);
+		SplitPane splitPane = new SplitPane(left, right);
+		splitPane.setDividerPosition(0, Constants.CHANNEL_LIST_WIDTH / Constants.STAGE_WIDTH);
 		root.setCenter(splitPane);
 		return root;
 	}
@@ -137,14 +146,14 @@ public class Main extends Application {
 			titledPane.setPrefWidth(Constants.CHANNEL_LIST_WIDTH);
 			accordion.getPanes().add(titledPane);
 			titledPane.setExpanded(false);
-			
+
 		}
 		accordion.expandedPaneProperty().addListener(new ChangeListener<TitledPane>() {
 
 			@Override
 			public void changed(ObservableValue<? extends TitledPane> observable, TitledPane oldValue,
 					TitledPane titledPane) {
-				
+
 				if (titledPane != null) {
 					titledPane.setContent(null);
 					TilePane pane = new TilePane();
@@ -170,23 +179,57 @@ public class Main extends Application {
 		return hbox;
 	}
 
+	private ImageView videoImageView = new ImageView();
+
 	private Node buildCenterPane() {
 		Pane node = new Pane();
-		ImageView videoImageView = new ImageView();
-		videoImageView.fitWidthProperty().bind(node.widthProperty());
-		videoImageView.fitHeightProperty().bind(node.heightProperty());
-		
 		embeddedMediaPlayer.videoSurface().set(ImageViewVideoSurfaceFactory.videoSurfaceForImageView(videoImageView));
-		
+
 		node.widthProperty().addListener((observableValue, oldValue, newValue) -> {
+			if (keepRatio) {
+				placeVideoImage(videoImageView, keepRatio);
+			} else {
+				videoImageView.fitHeightProperty().set(node.getHeight());
+				videoImageView.fitWidthProperty().set(newValue.doubleValue());
+			}
 		});
-		
 
 		node.heightProperty().addListener((observableValue, oldValue, newValue) -> {
+			if (!keepRatio) {
+				videoImageView.fitHeightProperty().set(node.getHeight());
+				videoImageView.fitWidthProperty().set(newValue.doubleValue());
+			} else {
+				placeVideoImage(videoImageView, keepRatio);
+			}
 		});
-			
+
 		node.getChildren().add(videoImageView);
 		return node;
+	}
+
+	private void placeVideoImage(ImageView imageView, boolean keepRatio) {
+		Region region = (Region) imageView.getParent();
+		double imageViewWidth = region.getWidth();
+		double imageViewHeight = region.getHeight();
+		if (keepRatio) {
+			IntByReference px = new IntByReference();
+			IntByReference py = new IntByReference();
+			LibVlc.libvlc_video_get_size(this.embeddedMediaPlayer.mediaPlayerInstance(), 0, px, py);
+			double videoWidth = px.getValue();
+			double videoHeight = py.getValue();
+			double scaleWidth = imageViewWidth / videoWidth;
+			double scaleHeight = imageViewHeight / videoHeight;
+			double imageViewWidthScaled = scaleWidth * videoWidth;
+			double imageViewHeightScaled = scaleWidth * videoHeight;
+			double y = (imageViewHeight - imageViewHeightScaled) / 2.0;
+			imageView.fitWidthProperty().set(imageViewWidthScaled);
+			imageView.fitHeightProperty().set(imageViewHeightScaled);
+			imageView.setX(0);
+			imageView.setY(y);
+		} else {
+			videoImageView.fitHeightProperty().set(imageViewWidth);
+			videoImageView.fitWidthProperty().set(imageViewWidth);
+		}
 	}
 
 	@Subscribe
