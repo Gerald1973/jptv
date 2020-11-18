c2871a5b0d69a854bbffb57624fc66bfb8f13673
Gérald Maréchal <gerald1973@gmail.com>
2020-11-18 19:18:23 +0100

    preferences button style ongoing

--- src/main/java/com/smilesmile1973/jptv/view/Preferences.java
+++ src/main/java/com/smilesmile1973/jptv/view/Preferences.java
@@ -10,15 +10,19 @@ import com.smilesmile1973.jptv.event.ChannelListCreatedEvent;
 import com.smilesmile1973.jptv.service.M3UService;
 import com.smilesmile1973.jptv.service.PreferencesService;
 
+import javafx.geometry.HPos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.stage.Window;
 
 /**
@@ -37,7 +41,10 @@ public class Preferences extends Stage {
 
 	private void init() {
 		buttonCancel = new Button("Cancel");
+		buttonCancel.getStyleClass().add("buttonPreferences");
 		buttonOk = new Button("Ok");
+		buttonOk.getStyleClass().add("buttonPreferences");
+		
 		lblUrl = new Label("URL M3U");
 		txtUrl = new TextField();
 		buttonCancel.setOnAction(actionEvent -> this.close());
@@ -67,14 +74,23 @@ public class Preferences extends Stage {
 	}
 
 	private Parent buildRoot() {
+		StackPane pane = new StackPane();
+		pane.setId("panePreferences");
 		GridPane node = new GridPane();
-		node.add(lblUrl, 0, 0);
-		node.add(txtUrl, 1, 0);
-		FlowPane flowPane = new FlowPane();
-		flowPane.getChildren().add(buttonOk);
-		flowPane.getChildren().add(buttonCancel);
-		node.add(flowPane, 0, 1, 2, 1);
-		return node;
+		ColumnConstraints cc0 = new ColumnConstraints();
+		ColumnConstraints cc1 = new ColumnConstraints();
+		ColumnConstraints cc2 = new ColumnConstraints();
+		cc2.setHalignment(HPos.RIGHT);
+		cc2.setHgrow(Priority.ALWAYS);
+	    node.setPrefWidth(600);
+		node.add(lblUrl, 0, 0,1,1);
+		node.add(txtUrl, 1, 0,2,1);
+		node.add(buttonOk, 0, 1);
+		node.add(buttonCancel, 2, 1);
+		node.getColumnConstraints().setAll(cc0,cc1,cc2);
+		pane.getChildren().add(node);
+		node.getColumnConstraints().setAll(cc0,cc1,cc2);
+		return pane;
 	}
 
 	public Preferences(Window owner) {
@@ -82,7 +98,9 @@ public class Preferences extends Stage {
 		this.initOwner(owner);
 		setTitle("Preferences");
 		Scene scene = new Scene(buildRoot());
+		scene.getStylesheets().add(getClass().getClassLoader().getResource("styles.css").toExternalForm());
 		this.setScene(scene);
+		this.initStyle(StageStyle.UNDECORATED);
 		this.initModality(Modality.WINDOW_MODAL);
 		this.showAndWait();
 	}
